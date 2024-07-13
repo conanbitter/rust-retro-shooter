f4d48b5843e6dea4692acefa75057c813eaedcb0
Conan Bitter <conan.bitter@gmail.com>
2024-07-13 10:13:04 +0300

    Add logo

--- palcalc/src/main.rs
+++ palcalc/src/main.rs
@@ -1,18 +1,32 @@
-use std::io::stdout;
+use std::io::{stdout, Write};
 
 use crossterm::{
-    execute,
+    cursor::{MoveTo, MoveToNextLine},
+    execute, queue,
     style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+    terminal::{Clear, ClearType},
 };
 
 fn main() {
-    println!("Hello, world!");
+    let mut stdout = stdout();
     execute!(
-        stdout(),
-        SetForegroundColor(Color::Blue),
+        stdout,
+        Clear(ClearType::All),
+        SetForegroundColor(Color::White),
         SetBackgroundColor(Color::Red),
-        Print("Styled text here."),
+        MoveTo(0, 1),
+        Print(r"  __   ___ ___  __   __      __        __   __  ___  ___  __   "),
+        MoveToNextLine(1),
+        Print(r" |__) |__   |  |__) /  \    /__` |__| /  \ /  \  |  |__  |__)  "),
+        MoveToNextLine(1),
+        Print(r" |  \ |___  |  |  \ \__/    .__/ |  | \__/ \__/  |  |___ |  \  "),
+        MoveToNextLine(1),
+        Print(r"                                                               "),
+        MoveToNextLine(1),
+        SetBackgroundColor(Color::DarkBlue),
+        Print(r"                      PALETTE CALCULATOR                       "),
         ResetColor
     )
     .unwrap();
+    stdout.flush().unwrap();
 }
