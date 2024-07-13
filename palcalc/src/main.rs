6270331fc6955af670f8a69d32c30eab962f117a
Conan Bitter <conan.bitter@gmail.com>
2024-07-13 13:31:19 +0300

    Improve UI

--- palcalc/src/main.rs
+++ palcalc/src/main.rs
@@ -1,33 +1,52 @@
-use std::io::{stdout, Write};
+use std::{
+    io::{stdout, Write},
+    thread,
+    time::Duration,
+};
 
 use crossterm::{
-    cursor::{MoveTo, MoveToNextLine},
-    execute, queue,
-    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
-    terminal::{self, Clear, ClearType},
+    cursor, execute, queue,
+    style::{self, Color},
+    terminal::{self, ClearType},
 };
 
 fn main() {
     let mut stdout = stdout();
-    let offset = (terminal::size().unwrap().0 - 63) / 2;
-    execute!(
+    let width = terminal::size().unwrap().0;
+    let offset = (width - 63) / 2;
+
+    terminal::enable_raw_mode().unwrap();
+    queue!(
         stdout,
-        Clear(ClearType::All),
-        SetForegroundColor(Color::White),
-        SetBackgroundColor(Color::Red),
-        MoveTo(offset, 1),
-        Print(r"  __   ___ ___  __   __      __        __   __  ___  ___  __   "),
-        MoveTo(offset, 2),
-        Print(r" |__) |__   |  |__) /  \    /__` |__| /  \ /  \  |  |__  |__)  "),
-        MoveTo(offset, 3),
-        Print(r" |  \ |___  |  |  \ \__/    .__/ |  | \__/ \__/  |  |___ |  \  "),
-        MoveTo(offset, 4),
-        Print(r"                                                               "),
-        MoveTo(offset, 5),
-        SetBackgroundColor(Color::DarkBlue),
-        Print(r"              P A L E T T E   C A L C U L A T O R              "),
-        ResetColor
+        terminal::EnterAlternateScreen,
+        terminal::Clear(ClearType::All),
+        style::SetForegroundColor(Color::White),
+        style::SetBackgroundColor(Color::Red),
+        cursor::Hide,
+        cursor::MoveTo(offset, 1),
+        style::Print(r"  __   ___ ___  __   __      __        __   __  ___  ___  __   "),
+        cursor::MoveTo(offset, 2),
+        style::Print(r" |__) |__   |  |__) /  \    /__` |__| /  \ /  \  |  |__  |__)  "),
+        cursor::MoveTo(offset, 3),
+        style::Print(r" |  \ |___  |  |  \ \__/    .__/ |  | \__/ \__/  |  |___ |  \  "),
+        cursor::MoveTo(offset, 4),
+        style::Print(r"                                                               "),
+        cursor::MoveTo(offset, 5),
+        style::SetBackgroundColor(Color::DarkBlue),
+        style::Print(r"              P A L E T T E   C A L C U L A T O R              "),
+        style::SetBackgroundColor(Color::Grey),
+        style::SetForegroundColor(Color::Black),
     )
     .unwrap();
+
+    for i in 7..11 {
+        queue!(stdout, cursor::MoveTo(0, i), terminal::Clear(ClearType::CurrentLine)).unwrap();
+    }
+
     stdout.flush().unwrap();
+
+    thread::sleep(Duration::from_secs(2));
+
+    execute!(stdout, cursor::Show, terminal::LeaveAlternateScreen).unwrap();
+    terminal::disable_raw_mode().unwrap();
 }
