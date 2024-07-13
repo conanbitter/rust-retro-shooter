58a692335b79ac98dfa06784db2447e9a5773e2d
Conan Bitter <conan.bitter@gmail.com>
2024-07-13 09:51:02 +0300

    Add crossterm

--- palcalc/src/main.rs
+++ palcalc/src/main.rs
@@ -1,3 +1,18 @@
+use std::io::stdout;
+
+use crossterm::{
+    execute,
+    style::{Color, Print, ResetColor, SetBackgroundColor, SetForegroundColor},
+};
+
 fn main() {
     println!("Hello, world!");
+    execute!(
+        stdout(),
+        SetForegroundColor(Color::Blue),
+        SetBackgroundColor(Color::Red),
+        Print("Styled text here."),
+        ResetColor
+    )
+    .unwrap();
 }
